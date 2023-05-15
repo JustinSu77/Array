da48b273b3ef32b03ccd93f0272297bab1947b5c
Justin <sujustin77@gmail.com>
2023-05-15 17:34:24 +0100

    Finish print function

--- Array.h
+++ Array.h
@@ -75,7 +75,15 @@ namespace
 
     }
 
-  
+    template<class T>
+    void Array<T>::print()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            std::cout << array[i] << " ";
+        }
+        std::cout << std::endl;
+    }
 }
 
 
