e9a3c427032d3bfe0ebaea788372237f3bc83290
Justin <sujustin77@gmail.com>
2023-05-17 00:40:42 +0100

    Commented code

--- Array.h
+++ Array.h
@@ -75,7 +75,7 @@ namespace justin_su
              * Copies elements from arr.array to array.
              * @param arr as existing instance of this class.
              */
-            Array(const Array& arr);
+            Array(const Array<T>& existingArray);
 
 
             /**
@@ -297,22 +297,7 @@ namespace justin_su
             int maxSize;
     };
 
-    template<class T>
-    void Array<T>::reverse()
-    {
-        if (size == 1 || isEmpty())
-            return;
-        int start = 0;
-        int end = size - 1;
-        while (start < end)
-        {
-            T temp = array[start];
-            array[start] = array[end];
-            array[end] = temp;
-            start++;
-            end--;
-        }
-    }
+
 
     /**
      * Default constructor.
@@ -321,118 +306,183 @@ namespace justin_su
     template<class T>
     Array<T>::Array(): size(0),maxSize(10)
     {
+        // Sets array pointer to be a dynamic array with maxSize
+        // Initialized to default value of T data type
         array = new T[maxSize]{};
 
     }
+
 
     template<class T>
     Array<T>::Array(int maxSize): size(0), maxSize(maxSize)
     {
+        // If given maxSize is negative throw exception
         if (maxSize < 0)
             throw std::runtime_error("Array(int maxSize): Given maxSize cannot be negative");
-        if (maxSize > INT_MAX)
-            throw std::runtime_error("Array(int maxSize): Given maxSize is too large! ");
+
+        // Sets array pointer to be a dynamic array with maxSize
+        // Initialized to default value of T data type
         array = new T[maxSize]{};
     }
 
     template<class T>
-
     Array<T>::Array(const std::initializer_list<T> &list): size(list.size()), maxSize(list.size())
     {
-        delete[] array;
+
+
+        // Reallocates array pointer to have maxSize and default values
         array = new T[maxSize]{};
+        // Set index counter to copy elements from list to array
         int index = 0;
-        for (auto element: list)
-        {
+        // Copy elements from range
+        for (auto element: list) {
+            // Set index of array to element of initializer list
             array[index] = element;
+            // Increment index counter
             index++;
         }
+
     }
 
     template<class T>
-    Array<T>::Array(const Array &arr)
+    Array<T>::Array(const Array<T>& existingArray):size(existingArray.size), maxSize(existingArray.maxSize)
     {
-        delete [] array;
-        size = arr.size;
-        maxSize = arr.maxSize;
-        for (int i = 0; i < size; i++)
-        {
-            array[i] = arr.array[i];
-        }
+        // Allocate memory for dynamic array
+         array = new T[maxSize]{};
+         // Copy elements from array of exising instance of this class
+         for (int i = 0; i < size; i++)
+         {
+             array[i] = existingArray.array[i];
+         }
+
+
     }
 
     template<class T>
     void Array<T>::print()
     {
+        // Print first bracket
+        std::cout << "[";
+        // Print the elements
         for (int i = 0; i < size; i++)
         {
-            std::cout << array[i] << " ";
+            // Last element will have no space before it
+            if (i == size - 1)
+                std::cout << array[i];
+            // Print space between elements otherwise
+            else
+                std::cout << array[i] << " ";
         }
+        // Print last bracket
+        std::cout << "]";
+        // Add newline to next output will be on next line
         std::cout << std::endl;
     }
 
     template<class T>
+    void Array<T>::reverse()
+    {
+        // If array only has one element or it is empty then return
+        if (size == 1 || isEmpty())
+            return;
+        // Set pointer to beginning of array
+        int start = 0;
+        // Set pointers to end of array
+        int end = size - 1;
+        // as long as start and end pointers do not meet each other
+        while (start < end)
+        {
+            // Swap
+            T temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            // Increase start pointers
+            start++;
+            // Decrease start pointers
+            end--;
+        }
+    }
+
+
+    template<class T>
     int Array<T>::length()
     {
+        // Return the size member variable
         return size;
     }
 
     template<class T>
     int Array<T>::capacity()
     {
+        // Return the maxSize member variable
         return maxSize;
     }
 
     template<class T>
     Array<T>::~Array()
     {
-        delete [] array;
+        // Deallocate dynamic array
+        delete[] array;
     }
 
     template<class T>
     void Array<T>::insertAtStart(T newValue)
     {
+        // If array is full, throw runtime_error
         if (isFull())
             throw std::runtime_error("insertAtStart: Array is full!");
+        // Shift elements to right first
         for (int i = size; i >= 0; i--)
         {
             array[i + 1] = array[i];
         }
+        // Insert given newValue at beginning
         array[0] = newValue;
+        // Increase size
         size++;
     }
 
     template<class T>
     void Array<T>::insertAtEnd(T newValue)
     {
+        // If array is full, throw runtime_error
         if (isFull())
             throw std::runtime_error("insertAtEnd: Array is full!");
+        // Set last element to newValue
         array[size] = newValue;
+        // Increase size by 1
         size++;
     }
 
     template<class T>
     void Array<T>::insertAt(int position,T newValue)
     {
-        if (position < 0 || position > size - 1)
+        // If given position is out of bounds, throw runtime error
+        if (position < 0 || position > size)
             throw std::runtime_error("insertAt: Array is full!");
+        // If array is full, throw runtime error
         if (isFull())
             throw std::runtime_error("insertAt: Array is full!");
+        // If position is 0, call insertAtStart function
         if (position == 0)
         {
             insertAtStart(newValue);
             return;
         }
-        if (position == size + 1)
+        // If position is at the end of array, call insertAtEnd function
+        if (position == size)
         {
             insertAtEnd(newValue);
             return;
         }
+        // Shift element at given position to right
         for (int i = size; i >= position; i--)
         {
             array[i + 1] = array[i];
         }
-        array[position] = newValue;
+        // Set given newValue to given position index - 1
+        array[position - 1] = newValue;
+        // Increase by 1
         size++;
 
 
@@ -441,111 +491,139 @@ namespace justin_su
     template<class T>
     void Array<T>::removeAtStart()
     {
+        // If array is full, throw runtime error
         if (isEmpty())
             throw std::runtime_error("removeAtStart: Array is already empty!");
+        // Shift elements to the left
         for (int i = 0; i < size; i++)
         {
             array[i] = array[i + 1];
         }
+        //Decrease size by 1
         size--;
     }
 
     template<class T>
     void Array<T>::removeAtEnd()
     {
+        // If array is full, throw runtime error
         if (isEmpty())
             throw std::runtime_error("removeAtEnd: Array is already empty!");
+        // Create a temp array to get the default value of the data type T
         T* temp = new T[2]{};
+        // Set the last element to be the default value
         array[size - 1] = temp[0];
+        // Deallocate temp array
         delete[] temp;
+        // Decrease size by 1
         size--;
     }
 
     template<class T>
     void Array<T>::removeAt(int position)
     {
+        // If given position is out of bounds, throw runtime_error
         if (position < 0 || position > size - 1)
             throw std::runtime_error("removeAt: Array is already empty!");
+        // If array is empty, throw runtime_error
         if (isEmpty())
             throw std::runtime_error("removeAt: Array is already empty!");
-
+        // If given position is 0, call removeAtStart function
         if (position == 0)
         {
             removeAtStart();
             return;
         }
+        // If given position is the last position, call removeAtEnd function
         if (position == size - 1)
         {
             removeAtEnd();
             return;
         }
-
+        // Decrease size by 1
         size--;
     }
 
     template<class T>
     int Array<T>::indexOf(T value)
     {
+        // Traverse array
         for (int i = 0; i < size; i++)
         {
+            // if the given value is found
             if (array[i] == value)
+                // Return the index
                 return i;
         }
+        // Otherwise return -1
         return -1;
     }
 
     template<class T>
     T Array<T>::front()
     {
+        // If array is empty, throw runtime_error
         if (isEmpty())
             throw std::runtime_error("front: Array is empty!");
+        // Return the first element
         return array[0];
     }
 
     template<class T>
     T Array<T>::back()
     {
+        // If array is empty, throw runtime_error
         if (isEmpty())
             throw std::runtime_error("back: Array is empty!");
+        // Return the last element
         return array[size - 1];
     }
 
     template<class T>
     T Array<T>::at(int position)
     {
-        if (position < 0 || position > size - 1);
+        if (position < 0 || position > size - 1)
             throw std::runtime_error("at: Given position is out of bounds!");
+        // Return the element at given position
         return array[position - 1];
     }
 
     template<class T>
     bool Array<T>::isFull()
     {
+        // Return if size == maxSize
         return size == maxSize;
     }
 
     template<class T>
     bool Array<T>::isEmpty()
     {
+        // Return if size is 0
         return size == 0;
     }
 
     template<class T>
     void Array<T>::sort()
     {
+        // Bubble sort
         for (int i = 0; i < size; i++)
         {
+            // Flag for optimization
             bool isSwapped = false;
             for (int j = 0; j < size - i - 1; j++)
             {
+                // If the next element is greater than current element
                 if (array[j + 1] > array[j])
                 {
+                    // Swap
                     T temp = array[j + 1];
                     array[j + 1] = array[j];
                     array[j] = temp;
-
+                    // Set flag to true
+                    isSwapped = true;
                 }
             }
+            // If flag has not been set then break
             if (!isSwapped)
                 break;
         }
@@ -555,54 +633,74 @@ namespace justin_su
     template<class T>
     void Array<T>::fill(T value)
     {
+        // Deallocate array pointer
         delete[] array;
+        // Reallocate array pointer
         array = new T[maxSize]{};
+        // Traverse the array to maxSize
         for (int i = 0; i < maxSize; i++)
         {
+            // Set the elements to given value
             array[i] = value;
         }
+        // Set size to maxSize
         size = maxSize;
     }
 
     template<class T>
     void Array<T>::clear()
     {
+        // Deallocate array pointer
         delete[] array;
+        // Set array elements to default elements
         array = new T[maxSize]{};
+        // Set size to 0
         size = 0;
 
     }
 
-    template<class T>
-    void Array<T>::operator=(const Array<T> &arr)
+    template <class T>
+    void Array<T>::operator=(const Array<T>& arr)
     {
+        // Set size to the size of arr.size
         size = arr.size;
-        maxSize = arr.size;
+        // Set size to arr.maxSize
+        maxSize = arr.maxSize;
+        // Deallocate array
+        delete[] array;
+        // Reallocate array pointer to default values
+        array = new T[maxSize]{};
+        // Copy element from arr.array to initialized array
         for (int i = 0; i < size; i++)
         {
             array[i] = arr.array[i];
         }
-
     }
 
     template<class T>
     bool Array<T>::operator==(const Array<T> &arr)
     {
+        // If size is not equal to arr.size or if maxSize is not equal to arr.maxSize
         if (size != arr.size || maxSize != arr.maxSize)
             return false;
+        // Check if any elements are different
         for (int i = 0; i < size; i++)
         {
             if (array[i] != arr.array[i])
                 return false;
         }
+        // Otherwise return true
         return true;
     }
+
 
     template<class T>
     T& Array<T>::operator[](int index)
     {
+        // If given index is out of bounds, throw runtime_error
         if (index < 0 || index > size)
             throw std::runtime_error("[]: Index is out of bounds!");
+        // Return the element at given index
         return  array[index - 1];
     }
 
--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace justin_su;
 int main()
 {
-    Array<int>arr;
+
 
     return 0;
 }
